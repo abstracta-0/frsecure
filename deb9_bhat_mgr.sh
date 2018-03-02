c28d91632f1dd17055e3df5ad12acf8cbc9da3d3
abstracta-0 <36171307+abstracta-0@users.noreply.github.com>
2018-03-02 13:56:21 -0600

    removed apt-get update && apt-get dist-upgrad -y

--- deb9_bhat_mgr.sh
+++ deb9_bhat_mgr.sh
@@ -4,8 +4,6 @@
 
 # dhclient
 
-apt-get update && apt-get dist-upgrade -y
-
 apt-get install -y tcl screen texlive-latex-base texlive-latex-extra texlive-latex-recommended net-tools build-essential cmake bison flex libpcap-dev pkg-config libglib2.0-dev libgpgme11-dev uuid-dev sqlfairy xmltoman doxygen libssh-dev libksba-dev libldap2-dev libsqlite3-dev libmicrohttpd-dev libxml2-dev libxslt1-dev xsltproc clang rsync rpm nsis alien sqlite3  libgcrypt20-dev libgnutls28-dev linux-headers-$(uname -r) python python-pip mingw-w64 heimdal-multidev libpopt-dev gnutls-bin certbot nmap ufw
 
 apt-get purge -y texlive-*-doc 
