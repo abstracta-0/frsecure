2e74504b0b5bd7ce0258d8caae84571344c293be
abstracta-0 <36171307+abstracta-0@users.noreply.github.com>
2018-03-02 13:32:43 -0600

    Delete redis_4_setup.sh

--- redis_4_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sed -i 's+port 6379+port 0+' /etc/redis/6379.conf
-sed -i 's+# unixsocket /tmp/redis.sock+unixsocket /var/run/redis/redis.sock+' /etc/redis/6379.conf
-
-sed -i 's+# unixsocketperm 700+unixsocketperm 700+' /etc/redis/6379.conf
-sed -i 's+REDISPORT="6379"+REDISPORT="0"+' /etc/init.d/redis_6379
-
-service redis_6379 restart
